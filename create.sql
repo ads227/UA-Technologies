714181d93a3a3c8c1100d016c4b05833345159e8
ads227 <ads227@uakron.edu>
2022-11-01 17:45:08 -0400

    Add create.sql

---
+++ create.sql
@@ -0,0 +1,29 @@
+create table event (
+    eventid int GENERATED ALWAYS AS IDENTITY,
+    title text,
+    category text,
+    hours real,
+    date date,
+    facilityName text,
+    facilityArea text,
+    overhead real,
+    rentalFee real,
+    primary key(eventid)
+);
+
+create table department (
+    departmentid GENERATED ALWAYS AS IDENTITY,
+    department text,
+    ytdearnings real,
+    primary key(departmentid)
+);
+
+create table eventstaffing (
+    departmentid integer,
+    eventid integer,
+    staff integer,
+    earnings real,
+    primary key(departmentid, eventid),
+    dep_fk foreign key(departmentid) references department(departmentid),
+    ev_fk foreign key(eventid) references event(eventid)
+);
