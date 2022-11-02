81009375501293db0beabebc31834a2426ca20a6
ads227 <ads227@uakron.edu>
2022-11-02 12:50:27 -0400

    Add update

--- app.py
+++ app.py
@@ -50,3 +50,38 @@ def append():
     cur.execute('INSERT INTO event( title,category,hours,date,facilityName,facilityArea,overhead,rentalFee) VALUES(%s, %s, %s, %s, %s, %s, %s, %s);', (title, category, hours, date, facility, area, overhead, fee))
     conn.commit()
     return render_template('index.html', success=True)
+
+@app.route('/update', methods = ['POST'])
+
+def update():
+    id = request.form.get('id')
+    title = request.form.get('title')
+    category = request.form.get('category')
+    hours = request.form.get('hours')
+    date = request.form.get('date')
+    facility = request.form.get('facility')
+    area = request.form.get('area')
+    overhead = request.form.get('overhead')
+    fee = request.form.get('fee')
+    return render_template('update.html', title = title, category = category, hours = hours, date = date, facility = facility, area = area, overhead = overhead, fee = fee, id=id)
+
+@app.route('/updateSubmit', methods = ['POST'])
+
+def updateSubmit():
+    id = request.form.get('id')
+    title = request.form.get('title')
+    category = request.form.get('category')
+    hours = request.form.get('hours')
+    date = request.form.get('date')
+    facility = request.form.get('facility')
+    area = request.form.get('area')
+    overhead = request.form.get('overhead')
+    fee = request.form.get('fee')
+    conn = psycopg2.connect("host=localhost dbname=events user=ads227 password=admin")
+    cur = conn.cursor()
+    cur.execute("UPDATE event SET title = %s, category = %s, hours = %s, date = %s, facilityname = %s, facilityarea = %s, overhead = %s, rentalfee = %s where eventid = %s;", (title, category, hours, date, facility, area, overhead, fee, id))
+    conn.commit()
+    cur.execute("SELECT * FROM event;")
+    contents = cur.fetchall()
+    rows = len(contents)
+    return render_template('display.html', content = contents, rows = rows)
