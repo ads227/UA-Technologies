dc6a898cb43acb1e230dc770bee356e0867efbcd
ads227 <ads227@uakron.edu>
2022-11-27 21:58:04 -0500

    Correct formatting

--- app/application/routes.py
+++ app/application/routes.py
@@ -34,23 +34,22 @@ def login():
 
 @app.route("/getAToken")  # Its absolute URL must match your app's redirect_uri set in AAD
 def authorized():
-    print("getAToken")
-    if request.args['state'] != session.get("state"):
-        return redirect(url_for("login"))
-    cache = _load_cache()
-    result = _build_msal_app(cache).acquire_token_by_authorization_code(
-        request.args['code'],
-        scopes=app_config.SCOPE,  # Misspelled scope would cause an HTTP 400 error here
-        redirect_uri=url_for("authorized", _external=True))
-    if "error" in result:
-        return "Login failure: %s, %s" % (
-            result["error"], result.get("error_description"))
-    session["user"] = result.get("id_token_claims")
-    _save_cache(cache)
-    return redirect(url_for("hello"))
+  print("getAToken")
+  if request.args['state'] != session.get("state"):
+    return redirect(url_for("login"))
+  cache = _load_cache()
+  result = _build_msal_app(cache).acquire_token_by_authorization_code(
+    request.args['code'],
+    scopes=app_config.SCOPE,  # Misspelled scope would cause an HTTP 400 error here
+    redirect_uri=url_for("authorized", _external=True))
+  if "error" in result:
+    return "Login failure: %s, %s" % (
+        result["error"], result.get("error_description"))
+  session["user"] = result.get("id_token_claims")
+  _save_cache(cache)
+  return redirect(url_for("hello"))
 
 @app.route('/display', methods = ['GET'])
-
 def data():
     if request.method == 'GET':
         conn = psycopg2.connect("host=localhost dbname=events user=ads227 password=admin")
@@ -61,12 +60,10 @@ def data():
         return render_template('display.html', content = contents, rows = rows)
 
 @app.route('/insert', methods = ['GET'])
-
 def insert():
     return render_template('insert.html')
 
 @app.route('/append', methods = ['POST'])
-
 def append():
     print(request.form["title"])
     title = request.form['title']
@@ -85,7 +82,6 @@ def append():
     return render_template('index.html', success=True)
 
 @app.route('/update', methods = ['POST'])
-
 def update():
     id = request.form.get('id')
     title = request.form.get('title')
@@ -99,7 +95,6 @@ def update():
     return render_template('update.html', title = title, category = category, hours = hours, date = date, facility = facility, area = area, overhead = overhead, fee = fee, id=id)
 
 @app.route('/updateSubmit', methods = ['POST'])
-
 def updateSubmit():
     id = request.form.get('id')
     title = request.form.get('title')
@@ -120,7 +115,6 @@ def updateSubmit():
     return render_template('display.html', content = contents, rows = rows)
 
 @app.route('/delete', methods = ['POST'])
-
 def delete():
     id = request.form.get('id')
     print(id)
@@ -134,12 +128,10 @@ def delete():
     return render_template('display.html', content = contents, rows = rows)
 
 @app.route('/search', methods = ['POST'])
-
 def search():
     return render_template('search.html')
 
 @app.route('/searchresult', methods = ['POST'])
-
 def result():
     title = request.form.get('title')
     print(title)
@@ -152,32 +144,32 @@ def result():
 
 @app.route("/logout")
 def logout():
-    session["user"] = None  # Log out from this app from its session
-    # session.clear()  # If you prefer, this would nuke the user's token cache too
-    return redirect(  # Also need to logout from Microsoft Identity platform
-        "https://login.microsoftonline.com/common/oauth2/v2.0/logout"
-        "?post_logout_redirect_uri=" + url_for("/", _external=True))
+  session["user"] = None  # Log out from this app from its session
+  # session.clear()  # If you prefer, this would nuke the user's token cache too
+  return redirect(  # Also need to logout from Microsoft Identity platform
+    "https://login.microsoftonline.com/common/oauth2/v2.0/logout"
+    "?post_logout_redirect_uri=" + url_for("/", _external=True))
 
 def _load_cache():
-    cache = msal.SerializableTokenCache()
-    if session.get("token_cache"):
-        cache.deserialize(session["token_cache"])
-    return cache
+  cache = msal.SerializableTokenCache()
+  if session.get("token_cache"):
+    cache.deserialize(session["token_cache"])
+  return cache
 
 def _save_cache(cache):
-    if cache.has_state_changed:
-        session["token_cache"] = cache.serialize()
+  if cache.has_state_changed:
+    session["token_cache"] = cache.serialize()
 
 def _build_msal_app(cache=None):
-    return msal.ConfidentialClientApplication(
-        app_config.CLIENT_ID, authority=app_config.AUTHORITY,
-        client_credential=app_config.CLIENT_SECRET, token_cache=cache)
+  return msal.ConfidentialClientApplication(
+    app_config.CLIENT_ID, authority=app_config.AUTHORITY,
+    client_credential=app_config.CLIENT_SECRET, token_cache=cache)
 
 def _get_token_from_cache(scope=None):
-    cache = _load_cache()  # This web app maintains one cache per session
-    cca = _build_msal_app(cache)
-    accounts = cca.get_accounts()
-    if accounts:  # So all account(s) belong to the current signed-in user
-        result = cca.acquire_token_silent(scope, account=accounts[0])
-        _save_cache(cache)
-        return result
+  cache = _load_cache()  # This web app maintains one cache per session
+  cca = _build_msal_app(cache)
+  accounts = cca.get_accounts()
+  if accounts:  # So all account(s) belong to the current signed-in user
+    result = cca.acquire_token_silent(scope, account=accounts[0])
+    _save_cache(cache)
+    return result
